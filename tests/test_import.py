d2f6073e91e1b989efab29462643dd2b728bdcc8
bloodearnest <bloodearnest@gmail.com>
2021-03-18 11:12:29 +0000

    Add tests PR action
    Refactor tests to use package metadata to figure out module names

--- tests/test_import.py
+++ tests/test_import.py
@@ -3,34 +3,29 @@ from importlib import import_module
 from pathlib import Path
 
 import pytest
-
-# map pypi package names to an importable module name
-MAPPINGS = {
-    "attrs": "attr",
-    "google-api-core": "google.api_core",
-    "google-auth": "google.auth",
-    "google-cloud-bigquery": "google.cloud.bigquery",
-    "google-cloud-core": "google.cloud",
-    "google-resumable-media": "google.resumable_media",
-    "googleapis-common-protos": "google.api",
-    "ipython": "IPython",
-    "opensafely-cohort-extractor": "cohortextractor",
-    "opensafely-jobrunner": "jobrunner",
-    "opensafely-matching": "osmatching",
-    "pillow": "PIL",
-    "pip-tools": "piptools",
-    "presto-python-client": "prestodb",
-    "protobuf": "google.protobuf",
-    "pyopenssl": "OpenSSL",
-    "python-dateutil": "dateutil",
-    "pyyaml": "yaml",
-    "pyzmq": "zmq",
-    "ruamel.yaml.clib": None,
-}
+from pkg_resources import Requirement, get_provider
 
 
-# generate list of expected packages to be able to install
-def generate_import_name(req_path):
+def get_module_names(pkg_name):
+    """Load pkg metadata to find out its importable module name(s)."""
+    modules = set()
+    provider = get_provider(Requirement.parse(pkg_name))
+    # top level package name is typically all we need
+    if provider.has_metadata("top_level.txt"):
+        modules |= set(provider.get_metadata_lines("top_level.txt"))
+    else:
+        # badly packaged dependency, make an educated guess
+        modules.add(pkg_name.replace("-", "_"))
+
+    if provider.has_metadata("namespace_packages.txt"):
+        modules |= set(provider.get_metadata_lines("namespace_packages.txt"))
+
+    # _ prefixed modules are typically C modules and not directly importable
+    return [n for n in modules if n[0] != "_"]
+
+
+def generate_import_names(req_path):
+    """Generate list of expected modules to be able to import."""
     with req_path.open() as fp:
         for line in fp:
             line = line.strip()
@@ -38,12 +33,13 @@ def generate_import_name(req_path):
                 continue
 
             name, _, _ = line.partition("==")
-            mapped_name = MAPPINGS.get(name, name.replace("-", "_"))
-            if mapped_name:
-                yield name, mapped_name
+            for module in get_module_names(name):
+                yield name, module
 
 
-@pytest.mark.parametrize("name, module", generate_import_name(Path("requirements.txt")))
+@pytest.mark.parametrize(
+    "name, module", generate_import_names(Path("requirements.txt"))
+)
 @pytest.mark.filterwarnings("ignore")
 def test_import_package(name, module):
     try:
